198e09ce049d19e95b250bf4295deb41637a029c
MuhammadAliGabol <aligabol2002@gmail.com>
2022-06-07 20:40:23 +0500

    added cors

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const bodyParser = require('body-parser')
 
 const authRouters = require('./routers/auth.routers');
 const userRouters = require('./routers/user.routers');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false })); 
 app.use(bodyParser.json());
 
